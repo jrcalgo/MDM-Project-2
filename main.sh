18caf2a7f506b249c5b4ece1a45c332caeae0974
Jackson Redman <95605366+jrcalgo@users.noreply.github.com>
2025-11-12 13:36:10 -0500

    Create main.sh to execute Python functions
    This script runs a series of Python functions for processing data.

---
+++ main.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# make sure .env is set correctly, requirements.txt has been installed, and python is >=3.8
+
+python searchFunction.py
+
+python ComparisionFunction.py
+
+python ValidatorFunction.py
+
+python Generate_CSV.py
+
+python Generate_final_output.py
